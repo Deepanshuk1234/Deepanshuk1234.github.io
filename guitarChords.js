b2210079fc5a3ffe022b79a6d1804939b2d5d97a
UdiAdi <udit.aditya94@gmail.com>
2017-01-11 23:46:05 +0530

    Update guitarChords.js

--- guitarChords.js
+++ guitarChords.js
@@ -31,12 +31,13 @@ $(document).ready(function(){
 		{
 			$("#selec").html("<h1>Its E-major<br></h1>");
 		}
-		/*
+		
 		if($(".hehe1").hasClass("c01") && $(".hehe2").hasClass("c02") && $(".hehe3").hasClass("c03") && 
 		   $(".hehe4").hasClass("c24") && $(".hehe5").hasClass("c35") && $(".hehe6").hasClass("c16") )
 		{
 			$("#selec").html("<h1>Its D-minor<br></h1>");
 		}
+		/*
 		if($(".hehe1").hasClass("c31") && $(".hehe2").hasClass("c22") && $(".hehe3").hasClass("c03") && 
 		   $(".hehe4").hasClass("c04") && $(".hehe5").hasClass("c35") && $(".hehe6").hasClass("c36") )
 		{
